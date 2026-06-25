9975416fbdd121a32b97f78e0fe6de9d5e7a4441
dharmik3193 <dnbambharoliya555@gmail.com>
2026-06-25 17:07:04 +0530

    db.js updated

--- config/db.js
+++ config/db.js
@@ -1,29 +1,30 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
 });
 
-async function testConnection() {
-    try {
-        const connection = await pool.getConnection();
-        console.log("✅ MySQL Connected Successfully");
-        connection.release();
-    } catch (err) {
-        console.error("❌ MySQL Connection Failed");
-        console.error(err.message);
-    }
-}
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-testConnection();
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    console.log("✅ Database Connected");
+    conn.release();
+  } catch (err) {
+    console.error("❌ DB Error:", err.code);
+    console.error(err.message);
+  }
+})();
 
 module.exports = pool;
