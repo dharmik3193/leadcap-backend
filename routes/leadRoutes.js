c21c94f418d62709fbe6929311a9d19265d2ee70
dharmik3193 <dnbambharoliya555@gmail.com>
2026-06-27 20:23:03 +0530

    controller updated

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -3,9 +3,27 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAllLeads
+
+    getAllLeads,
+
+    getLeadById,
+
+    updateStatus,
+
+    updateNotes,
+
+    deleteLead
+
 } = require("../controllers/leadController");
 
 router.get("/", getAllLeads);
+
+router.get("/:id", getLeadById);
+
+router.put("/:id/status", updateStatus);
+
+router.put("/:id/notes", updateNotes);
+
+router.delete("/:id", deleteLead);
 
 module.exports = router;
