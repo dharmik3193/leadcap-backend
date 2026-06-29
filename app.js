3d1abb14a2b1e04ab9b157e88ac7baaa92043a63
dharmik3193 <dnbambharoliya555@gmail.com>
2026-06-29 14:13:04 +0530

    cors added

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2/promise');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Database Connection Pool
 const db = mysql.createPool({
